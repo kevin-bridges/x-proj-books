095b38526cf6d7b6b336d12a417f4586a696a55d
Kevin Bridges <kevin.b.bridges@gmail.com>
2018-10-26 08:40:47 -0400

    updating for heroku

--- server/models/books.js
+++ server/models/books.js
@@ -2,24 +2,26 @@ const mongoose = require('mongoose');
 
 
 const bookSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    author:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    pages:Number,
-    price:Number,
-    stores:{
-        type:[],
-        default:null
-    }
+  name:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  author:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  pages:Number,
+  price:Number,
+  stores:{
+    type:[],
+    default:null
+  }
 })
 
-const Book = mongoose.model('Book',bookSchema);
+
+const Book = mongoose.model('Book', bookSchema);
+
 
 module.exports = {Book}
--- server/models/stores.js
+++ server/models/stores.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
 
 const storeSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    address:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    phone:Number
+  name:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  address:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  phone:Number
 })
 
+
 const Store = mongoose.model('Store', storeSchema);
+
 
 module.exports = {Store}
